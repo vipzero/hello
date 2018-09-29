3a87a9b141257a1d38a6e91da67275a39e0c3873
vipzero <hiroflighter@gmail.com>
2018-09-29 13:44:38 +0900

    Jk hide

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,24 @@
 // @flow
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import Home from '../../containers/Home'
+import Jiko from '../../containers/Jiko'
+
+const MenuItem = styled(Link)`
+	margin-left: 10px;
+`
 
 const App = () => (
 	<Router>
 		<div>
+			<div>
+				<MenuItem to="/uji">uji</MenuItem>
+				<MenuItem to="/jiko">jiko</MenuItem>
+			</div>
 			<Route exact path="/" component={Home} />
+			<Route exact path="/jiko" component={Jiko} />
 		</div>
 	</Router>
 )
---
+++ src/containers/Jiko/index.js
@@ -0,0 +1,111 @@
+// @flow
+import * as React from 'react'
+import { connect, type Connector } from 'react-redux'
+
+import styled, { keyframes } from 'styled-components'
+
+import type { State as RootState } from '../../types'
+import HikinigeImg from '../../components/jiko_car_hikinige.png'
+import JkImg from '../../components/smartphone_schoolgirl_stand_smile.png'
+import PlayerImg from '../../components/figure_tousenbo.png'
+
+const Wrap = styled.div``
+const Top = styled.div`
+	width: 50%;
+`
+
+const move = keyframes`
+  0% {
+		left: 0;
+		top: 0;
+  }
+  25% {
+		left: 100%;
+		top: 0;
+	}
+  50% {
+		left: 100%;
+		top: 100%;
+	}
+
+  75% {
+		left: 0;
+		top: 100%;
+  }
+
+  100% {
+		left: 0;
+		top: 0;
+  }
+`
+
+const Hikinige = styled.img`
+	width: 50%;
+`
+
+const Player = styled.img`
+	width: 50%;
+	position: absolute;
+	left: ${p => p.x - 100}px;
+`
+
+const JkDiv = styled.div`
+	position: relative;
+	width: 1000px;
+	height: 1000px;
+	overflow: hidden;
+`
+
+const PlayerArea = styled.div`
+	position: relative;
+	height: 200px;
+	width: 100%;
+`
+
+const Jk = styled.img`
+	position: absolute;
+	width: 200px;
+	animation: ${move} 3s linear infinite;
+`
+
+type Props = {}
+
+class Home extends React.Component<Props> {
+	constructor(props) {
+		super(props)
+		this.state = { x: 0, y: 0 }
+	}
+	componentDidMount() {}
+
+	_onMouseMove(e) {
+		console.log(e)
+		console.log(e.clientX, e.clientY)
+		this.setState({ x: e.clientX, y: e.clientY })
+	}
+
+	render() {
+		const { state } = this
+		return (
+			<Wrap>
+				<Top>
+					<Hikinige src={HikinigeImg} alt="" />
+					<PlayerArea onMouseMove={this._onMouseMove.bind(this)}>
+						<Player x={state.x} y={state.y} src={PlayerImg} alt="" />
+					</PlayerArea>
+					<JkDiv>
+						<Jk src={JkImg} alt="" />
+					</JkDiv>
+				</Top>
+			</Wrap>
+		)
+	}
+}
+
+const ms = (state: RootState) => ({})
+
+const conn: Connector<{}, Props> = connect(
+	ms,
+	{},
+)
+
+export default conn(Home)
