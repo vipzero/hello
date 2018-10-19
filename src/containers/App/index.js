cc739d02e42881999d64f5fb236543354f213326
vipzero <hiroflighter@gmail.com>
2018-10-19 11:20:24 +0900

    Add ranker

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,11 @@
 // @flow
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import React from 'react'
 import styled from 'styled-components'
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import Home from '../../containers/Home'
 import Jiko from '../../containers/Jiko'
+import MakeRank from '../MakeRank'
 
 const MenuItem = styled(Link)`
 	margin-left: 10px;
@@ -16,9 +17,11 @@ const App = () => (
 			<div>
 				<MenuItem to="/uji">uji</MenuItem>
 				<MenuItem to="/jiko">jiko</MenuItem>
+				<MenuItem to="/makerank">makerank</MenuItem>
 			</div>
 			<Route exact path="/" component={Home} />
 			<Route exact path="/jiko" component={Jiko} />
+			<Route exact path="/makerank" component={MakeRank} />
 		</div>
 	</Router>
 )
---
+++ src/containers/MakeRank/index.js
@@ -0,0 +1,105 @@
+// @flow
+import * as React from 'react'
+import { connect, type Connector } from 'react-redux'
+import _ from 'lodash'
+
+import styled from 'styled-components'
+
+import type { State as RootState } from '../../types'
+
+const Wrap = styled.div``
+const Top = styled.div`
+	width: 50%;
+`
+
+const titles = ['vipper', 'うんこ', 'ちんこ']
+type State = {
+	cells: boolean,
+}
+
+class MakeRank extends React.Component<Props, State> {
+	state = {
+		cells: {
+			0: { 0: false, 1: true, 2: true },
+			1: { 0: false, 1: false, 2: true },
+			2: { 0: false, 1: false, 2: false },
+		},
+	}
+	componentDidMount() {}
+
+	render() {
+		const { state } = this
+		const nums = _.range(3).map(i => ({
+			n: _.sum(_.values(state.cells[i])),
+			title: titles[i],
+		}))
+		const rank = _.reverse(_.sortBy(nums, 'n'))
+		console.log(rank)
+		return (
+			<Wrap>
+				<table>
+					<tr>
+						<th>強いのどっち</th>
+						{_.range(3).map(x => (
+							<th key={x}>{titles[x]}</th>
+						))}
+					</tr>
+					{_.range(3).map(x => (
+						<tr key={x}>
+							<th>{titles[x]}</th>
+							{_.range(3).map(
+								y =>
+									x === y ? (
+										<td key={y}>-</td>
+									) : (
+										<td
+											key={y}
+											style={{
+												width: '100px',
+												height: '100px',
+												fontSize: '3em',
+											}}
+											onClick={() => {
+												this.setState({
+													cells: {
+														...state.cells,
+														[x]: {
+															...state.cells[x],
+															[y]: !state.cells[x][y],
+														},
+														[y]: {
+															...state.cells[y],
+															[x]: !state.cells[y][x],
+														},
+													},
+												})
+											}}
+										>
+											{state.cells[x][y] ? 'o' : 'x'}
+										</td>
+									),
+							)}
+						</tr>
+					))}
+				</table>
+				<ul>
+					{_.map(rank, (v, i) => (
+						<li>
+							{i + 1}
+							胃: {v.title} - {v.n}勝
+						</li>
+					))}
+				</ul>
+			</Wrap>
+		)
+	}
+}
+
+const ms = (state: RootState) => ({})
+
+const conn: Connector<{}, Props> = connect(
+	ms,
+	{},
+)
+
+export default conn(MakeRank)
