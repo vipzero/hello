dba6bac38443aa6ec5814b07958161bfe46c776c
vipzero <hiroflighter@gmail.com>
2019-07-08 01:19:15 +0900

    update graph range

--- src/containers/Probability/index.tsx
+++ src/containers/Probability/index.tsx
@@ -16,18 +16,38 @@ type Plot = { x: number; y: number }
 function geometricProgressionSum(a: number, r: number, n: number) {
 	return (a * (1 - Math.pow(r, n))) / (1 - r)
 }
+const toPlot = (x: number): Plot => ({ x, y: proOneHundred(x) * 100 })
 
 const proOneHundred = _.partial(geometricProgressionSum, 1 / 100, 99 / 100)
-const dataA: Plot[] = _.range(0, 501).map(i => ({
-	x: i,
-	y: proOneHundred(i) * 100,
-}))
+const dataA: Plot[] = [..._.range(10), ..._.range(10, 501, 10)].map(toPlot)
 
 const proOneTenth = _.partial(geometricProgressionSum, 1 / 10, 9 / 10)
-const dataB: Plot[] = _.range(0, 101).map(i => ({
+const dataB: Plot[] = [..._.range(50), ..._.range(50, 101, 10)].map(i => ({
 	x: i,
 	y: proOneTenth(i) * 100,
 }))
+
+function Graph({ data }: { data: Plot[] }) {
+	return (
+		<ResponsiveContainer width="100%" aspect={8 / 3}>
+			<LineChart data={data}>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis type="number" dataKey="x" domain={['dataMin', 'dataMax']} />
+				<YAxis unit="%" />
+				<Tooltip />
+				<Legend />
+				<Line
+					isAnimationActive={false}
+					type="monotone"
+					dataKey="y"
+					label={false}
+					stroke="#8884d8"
+					activeDot={{ r: 8 }}
+				/>
+			</LineChart>
+		</ResponsiveContainer>
+	)
+}
 
 function Probability() {
 	return (
@@ -36,41 +56,12 @@ function Probability() {
 				<h2>コンマ3桁ゾロ目確率</h2>
 				<h3>一回(1/100とする)</h3>
 
-				<ResponsiveContainer width="100%" aspect={8 / 3}>
-					<LineChart data={dataA}>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="x" />
-						<YAxis unit="%" />
-						<Tooltip />
-						<Legend />
-						<Line
-							type="monotone"
-							dataKey="y"
-							stroke="#8884d8"
-							activeDot={{ r: 8 }}
-						/>
-					</LineChart>
-				</ResponsiveContainer>
+				<Graph data={dataA} />
 			</div>
 			<div>
 				<h2>コンマ2桁ゾロ目確率</h2>
 				<h3>一回(1/10とする)</h3>
-				<ResponsiveContainer width="100%" aspect={8 / 3}>
-					<LineChart data={dataB}>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="x" />
-						<YAxis unit="%" />
-						<Tooltip />
-						<Legend />
-						<Line
-							type="monotone"
-							dataKey="y"
-							label={false}
-							stroke="#8884d8"
-							activeDot={{ r: 8 }}
-						/>
-					</LineChart>
-				</ResponsiveContainer>
+				<Graph data={dataB} />
 			</div>
 		</div>
 	)
