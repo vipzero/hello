57674e4876f07b8080477a52b841d946b0b83bce
vipzero <hiroflighter@gmail.com>
2019-07-27 12:02:17 +0900

    add zorome graph

--- src/containers/Probability/index.tsx
+++ src/containers/Probability/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import _ from 'lodash'
+import { Decimal } from 'decimal.js'
+
 import {
 	LineChart,
 	Line,
@@ -31,6 +33,30 @@ const dataHandred: Plot[] = [..._.range(100), ..._.range(100, 501, 10)].map(
 const dataThousand: Plot[] = [..._.range(100), ..._.range(100, 5001, 100)].map(
 	proOneThousand,
 )
+
+function permutation(n, r) {
+	return _.range(0, r).reduce((d, i) => d.times(n - i), new Decimal(1))
+}
+
+function combination(n, r) {
+	return permutation(n, r).div(permutation(r, r))
+}
+function repeatTry(n, k, p) {
+	const a = combination(n, k)
+	const b = Decimal.pow(p, k)
+	const c = Decimal.pow(1 - p, n - k)
+	// console.log({ a: a.toFixed(4), b: b.toFixed(4), c: c.toFixed(4) })
+	// console.log({ a, b, c })
+	return a.times(b).times(c)
+}
+
+console.log(repeatTry(10, 1, 0.5).toFixed(10))
+// const res = _.range(5).map(i => repeatTry(5, i, 0.5))
+// コンマゾロメが i 回でる確率
+const dataZorome: Plot[] = _.range(30).map(x => ({
+	x,
+	y: Number(repeatTry(1000, x, 0.01).toFixed(5)) * 100,
+}))
 
 function Graph({ data }: { data: Plot[] }) {
 	return (
@@ -71,6 +97,11 @@ function Probability() {
 				<h2>特定のコンマ確率</h2>
 				<h3>一回(1/1000とする)</h3>
 				<Graph data={dataThousand} />
+			</div>
+			<div>
+				<h2>1000レスでコンマゾロ目がx回出る確率</h2>
+				<h3>ゾロ目(1/100とする)</h3>
+				<Graph data={dataZorome} />
 			</div>
 		</div>
 	)
