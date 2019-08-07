04ad0ae15c93079f5010fae474fb0583a5336031
vipzero <hiroflighter@gmail.com>
2019-08-08 02:02:42 +0900

    add 分布

--- src/containers/Probability/index.tsx
+++ src/containers/Probability/index.tsx
@@ -16,10 +16,14 @@ import { Typography } from '@material-ui/core'
 
 type Plot = { x: number; y: number }
 
-const geometricProgressionSumPlot = (r: number, x: number) => ({
-	x,
-	y: (1 - Math.pow(r, x)) * 100,
-})
+const geometricProgressionSumPlot = (r: number, x: number) => {
+	const y = 1 - Math.pow(r, x)
+	return {
+		x,
+		y: y * 100,
+		z: y * Math.pow(r, x + 1) * 100,
+	}
+}
 
 const genProOneX = (n: number) =>
 	_.partial(geometricProgressionSumPlot, 1 - 1 / n)
@@ -81,6 +85,38 @@ function Graph({ data, unit = '%' }: { data: Plot[]; unit?: string }) {
 	)
 }
 
+function Graph2({ data, unit = '%' }: { data: Plot[]; unit?: string }) {
+	return (
+		<ResponsiveContainer width="100%" aspect={8 / 3}>
+			<LineChart data={data}>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis type="number" dataKey="x" domain={['dataMin', 'dataMax']} />
+				<YAxis unit={unit} />
+				<Tooltip />
+				<Legend />
+				<Line
+					isAnimationActive={false}
+					type="monotone"
+					dataKey="y"
+					label={false}
+					name="到達する確率"
+					stroke="#8884d8"
+					activeDot={{ r: 8 }}
+				/>
+				<Line
+					isAnimationActive={false}
+					type="monotone"
+					dataKey="z"
+					name="で終わる確率"
+					label={false}
+					stroke="red"
+					activeDot={{ r: 8 }}
+				/>
+			</LineChart>
+		</ResponsiveContainer>
+	)
+}
+
 const measureHoliday = h => {
 	if (h < 2) return 10
 	if (h < 4) return 20
@@ -90,14 +126,14 @@ const measureHoliday = h => {
 	return 5
 }
 
-const measureDay = h => {
-	if (h < 2) return 15
-	if (h < 4) return 25
-	if (h < 9) return 45
-	if (h < 16) return 25
-	if (h < 19) return 15
-	return 5
-}
+// const measureDay = h => {
+// 	if (h < 2) return 15
+// 	if (h < 4) return 25
+// 	if (h < 9) return 45
+// 	if (h < 16) return 25
+// 	if (h < 19) return 15
+// 	return 5
+// }
 const dataHourSpeed: Plot[] = []
 _.range(24).reduce((p, h) => {
 	const hourCount = 60 / measureHoliday(h)
@@ -106,6 +142,8 @@ _.range(24).reduce((p, h) => {
 	return y
 }, 0)
 
+console.log(dataHandred)
+
 function Probability() {
 	return (
 		<div>
@@ -113,17 +151,17 @@ function Probability() {
 			<div>
 				<h2>レス回数 x コンマ2桁ゾロ目</h2>
 				<h3>一回(1/10とする)</h3>
-				<Graph data={dataTen} />
+				<Graph2 data={dataTen} />
 			</div>
 			<div>
 				<h2>レス回数 x コンマ3桁ゾロ目</h2>
 				<h3>一回(1/100とする)</h3>
-				<Graph data={dataHandred} />
+				<Graph2 data={dataHandred} />
 			</div>
 			<div>
 				<h2>レス回数 x 特定のコンマ</h2>
 				<h3>一回(1/1000とする)</h3>
-				<Graph data={dataThousand} />
+				<Graph2 data={dataThousand} />
 			</div>
 			<div>
 				<h2>1000レスでコンマゾロ目がx回出る確率</h2>
