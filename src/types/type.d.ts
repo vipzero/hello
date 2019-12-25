5eee5c1fa17f156873320eb424b8228d7f57cf30
vipzero <hiroflighter@gmail.com>
2019-12-25 11:57:31 +0900

    update packages

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,6 +1,7 @@
 declare module '*.png'
 
 declare module 'noisejs' {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	class Noise {
 		constructor(seed: number)
 		perlin2(x: number, y: number): number
