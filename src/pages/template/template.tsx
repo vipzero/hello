b75975eca01e31b23fd502eccdc99b126984c350
vipzero <hiroflighter@gmail.com>
2020-02-08 19:26:10 +0900

    update template

---
+++ src/pages/template/template.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Button } from '@material-ui/core'
+import Layout from '../../components/Layout'
+
+function Base() {
+	const [count, setCount] = useState<number>(0)
+
+	return (
+		<div>
+			<Button onClick={() => setCount(c => c + 1)}>+1</Button>
+			{count}
+		</div>
+	)
+}
+
+const BasePage = () => (
+	<Layout title="タイトル">
+		<Base />
+	</Layout>
+)
+
+export default BasePage
