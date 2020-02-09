dc6a4c870ef1bdd7f2a0928b487a8fec518b64db
vipzero <hiroflighter@gmail.com>
2020-02-09 15:39:44 +0900

    add toc

--- src/components/Shomona22222/index.tsx
+++ src/components/Shomona22222/index.tsx
@@ -10,11 +10,43 @@ function Shomona22222() {
 		<Style>
 			<header>
 				<h1>SHOMONA CUP 2020年令和2年2月第2回</h1>
+				<h3>2/9(日) 21:00</h3>
 			</header>
+			<div>
+				<h2>大会ルール</h2>
+			</div>
+			<div>
+				<h2>チーム</h2>
+			</div>
+			<div>
+				<h2>結果</h2>
+			</div>
+			<div>
+				<h2>結果</h2>
+			</div>
 			<footer>
 				<ul>
 					<li>
-						<a href="">スレ: TODO</a>
+						<a href="http://hebi.5ch.net/test/read.cgi/news4vip/1581209829/l50">
+							スレ
+						</a>
+					</li>
+					<li>
+						<a href="">配信？</a>
+					</li>
+					<li>
+						<a href="https://shomona.vipper.dev/">2019-05 しょうもな杯</a>
+					</li>
+					<li>
+						<a href="https://tsuyunojin-splatoon2019.vipper.dev">
+							2019-06 VIPダービー梅雨の陣
+						</a>
+					</li>
+					<li>
+						<a href="https://hello.vipper.dev">0000-00 vip0</a>
+					</li>
+					<li>
+						<a href="https://google.com">主催: やまおか</a>
 					</li>
 				</ul>
 			</footer>
