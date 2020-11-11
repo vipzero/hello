0d5d895daacea64a1eb858f236c399be2d079b76
vipzero <hiroflighter@gmail.com>
2020-11-11 18:33:16 +0900

    update package, config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,27 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => (
+	<>
+		<Head>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1, shrink-to-fit=no"
+			/>
+			<link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
+			<meta name="theme-color" content="#000" key="themeColor" />
+			<link rel="manifest" href="/manifest.json" />
+			<link
+				href="https://fonts.googleapis.com/css?family=Nova+Mono"
+				rel="stylesheet"
+			/>
+			<link
+				href="https://fonts.googleapis.com/css?family=Major+Mono+Display"
+				rel="stylesheet"
+			/>
+		</Head>
+		<Component {...pageProps} />
+	</>
+)
 
 export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,7 +23,8 @@ class Document extends NextDocument<Props> {
 		try {
 			ctx.renderPage = () =>
 				originalRenderPage({
-					enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
 				})
 
 			const initialProps = await NextDocument.getInitialProps(ctx)
@@ -47,23 +48,7 @@ class Document extends NextDocument<Props> {
 	render() {
 		return (
 			<Html lang={'ja'}>
-				<Head>
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1, shrink-to-fit=no"
-					/>
-					<link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
-					<meta name="theme-color" content="#000" key="themeColor" />
-					<link rel="manifest" href="/manifest.json" />
-					<link
-						href="https://fonts.googleapis.com/css?family=Nova+Mono"
-						rel="stylesheet"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css?family=Major+Mono+Display"
-						rel="stylesheet"
-					/>
-				</Head>
+				<Head />
 
 				<body>
 					<Main />
