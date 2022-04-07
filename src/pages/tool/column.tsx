57ce87edb2c0ca903380be3e26bd02418b0afaee
vipzero <hiroflighter@gmail.com>
2022-04-07 21:41:36 +0900

    make view

--- src/pages/tool/column.tsx
+++ src/pages/tool/column.tsx
@@ -1,7 +1,14 @@
-// eslint-disable-next-line
-import { Button, Container, Slider, TextField, Typography } from '@mui/material'
+import {
+	Box,
+	Container,
+	Grid,
+	Slider,
+	TextField,
+	Typography,
+} from '@mui/material'
 import { range } from 'lodash'
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
+import { useLocalStorage } from 'react-use'
 import styled from 'styled-components'
 import TinySegmenter from 'tiny-segmenter'
 import Layout from '../../components/Layout'
@@ -14,11 +21,16 @@ const genText = (l1: string, l2: string, l3: string) =>
 
 const rand = (n: number) => Math.floor(Math.random() * n)
 function Ruru() {
-	const [text, setText] = useState<string>('')
+	const [text, setText] = useLocalStorage<string>(
+		'column-text',
+		'あいうえお\nかきくけこ\nさしすせそ'
+	)
 	const [col, setCol] = useState<number>(2)
 
 	const onChange = (v) => setText(v.currentTarget.value)
-	const cn = text.split('あいうえお\nかきくけこ\nさしすせそ').length
+	if (text === undefined) return null
+	const cn = text.split('').length
+	const result = text
 
 	return (
 		<Layout title="2列テキストツール">
@@ -30,19 +42,31 @@ function Ruru() {
 						defaultValue={col}
 						aria-labelledby="discrete-slider"
 						valueLabelDisplay="auto"
-						marks={range(2, 10 + 1).map((v) => ({ value: v, label: `${v}列` }))}
+						marks={range(2, 5 + 1).map((v) => ({ value: v, label: `${v}列` }))}
 						onChange={(_e, v) => {
 							if (typeof v === 'number') setCol(v)
 						}}
 						min={2}
-						max={10}
+						max={5}
 					/>
-					<TextField
-						label="リスト"
-						multiline
-						variant="outlined"
-						onChange={onChange}
-					/>
+					<Grid container spacing={{ xs: 1, md: 2 }}>
+						<Grid item xs={10} sm={6} md={6}>
+							<TextField
+								label="リスト"
+								multiline
+								defaultValue={text}
+								variant="outlined"
+								onChange={onChange}
+							/>
+						</Grid>
+						<Grid item xs={10} sm={6} md={6}>
+							<div className="result">
+								<code>
+									<pre>{result}</pre>
+								</code>
+							</div>
+						</Grid>
+					</Grid>
 				</Style>
 			</Container>
 		</Layout>
@@ -55,10 +79,10 @@ const Style = styled.div`
 		padding: 4px;
 		margin: 4px; */
 	}
-	.rurus {
-		display: flex;
+	.result {
 		flex-wrap: wrap;
 		gap: 1rem;
+		font-size: 0.6rem;
 		> div {
 			border: solid 1px gray;
 			padding: 0.5rem;
